461ac48ce534367e64ff029423dd494fcf5f2345
magikarpow <133470530+magikarpow@users.noreply.github.com>
2024-01-11 05:41:35 -0500

    Update Route7.asm

--- data/wild/maps/Route7.asm
+++ data/wild/maps/Route7.asm
@@ -2,15 +2,15 @@ Route7WildMons:
 	def_grass_wildmons 15 ; encounter rate
 	db 19, PIDGEY
 IF DEF(_RED)
-	db 19, ODDISH
+	db 19, BELLSPROUT
 	db 17, MANKEY
 	db 22, ODDISH
 	db 22, PIDGEY
-	db 18, MANKEY
-	db 18, GROWLITHE
+	db 18, MEOWTH
+	db 18, VULPIX
 	db 20, GROWLITHE
-	db 19, MANKEY
-	db 20, MANKEY
+	db 19, JIGGLYPUFF
+	db 20, JIGGLYPUFF
 ENDC
 IF DEF(_BLUE)
 	db 19, BELLSPROUT
