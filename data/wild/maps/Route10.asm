2882d2dacd376a88707e224b00c44b07a9cb5308
magikarpow <133470530+magikarpow@users.noreply.github.com>
2024-01-11 05:30:14 -0500

    Update Route10.asm

--- data/wild/maps/Route10.asm
+++ data/wild/maps/Route10.asm
@@ -2,15 +2,15 @@ Route10WildMons:
 	def_grass_wildmons 15 ; encounter rate
 	db 16, VOLTORB
 	db 16, SPEAROW
-	db 14, VOLTORB
+	db 14, MAGNEMITE
 IF DEF(_RED)
 	db 11, EKANS
 	db 13, SPEAROW
 	db 15, EKANS
 	db 17, VOLTORB
-	db 17, SPEAROW
-	db 13, EKANS
-	db 17, EKANS
+	db 17, MAGNEMITE
+	db 13, MACHOP
+	db 17, MACHOP
 ENDC
 IF DEF(_BLUE)
 	db 11, SANDSHREW
