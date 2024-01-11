d0d7dff5c26113dbf566747bde29713024a4b553
magikarpow <133470530+magikarpow@users.noreply.github.com>
2024-01-10 23:51:44 -0500

    Update ViridianForest.asm

--- data/wild/maps/ViridianForest.asm
+++ data/wild/maps/ViridianForest.asm
@@ -2,13 +2,13 @@ ViridianForestWildMons:
 	def_grass_wildmons 8 ; encounter rate
 IF DEF(_RED)
 	db  4, WEEDLE
+	db  4, CATERPIE
 	db  5, KAKUNA
-	db  3, WEEDLE
 	db  5, WEEDLE
-	db  4, KAKUNA
-	db  6, KAKUNA
+	db  5, CATERPIE
+	db  6, METAPOD
 	db  4, METAPOD
-	db  3, CATERPIE
+	db  3, PIKACHU
 ENDC
 IF DEF(_BLUE)
 	db  4, CATERPIE
@@ -20,8 +20,8 @@ IF DEF(_BLUE)
 	db  4, KAKUNA
 	db  3, WEEDLE
 ENDC
-	db  3, PIKACHU
-	db  5, PIKACHU
+	db  4, PIDGEY
+	db  6, PIDGEOTTO
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
