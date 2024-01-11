6f22956582da3ba08dfcb1364707db20b7ada2ef
magikarpow <133470530+magikarpow@users.noreply.github.com>
2024-01-11 05:39:12 -0500

    Update Route8.asm

--- data/wild/maps/Route8.asm
+++ data/wild/maps/Route8.asm
@@ -1,16 +1,16 @@
 Route8WildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 18, PIDGEY
+	db 18, MEOWTH
 IF DEF(_RED)
 	db 18, MANKEY
-	db 17, EKANS
+	db 17, PIDGEY
 	db 16, GROWLITHE
-	db 20, PIDGEY
-	db 20, MANKEY
-	db 19, EKANS
+	db 16, VULPIX
+	db 19, ABRA
+	db 17, VULPIX
 	db 17, GROWLITHE
-	db 15, GROWLITHE
-	db 18, GROWLITHE
+	db 20, KADABRA
+	db 22, KADABRA
 ENDC
 IF DEF(_BLUE)
 	db 18, MEOWTH
