cc5e00d96501f084657017e2d75ef7f7179c1ec1
magikarpow <133470530+magikarpow@users.noreply.github.com>
2024-01-10 23:23:43 -0500

    Update Route22.asm

--- data/wild/maps/Route22.asm
+++ data/wild/maps/Route22.asm
@@ -2,14 +2,14 @@ Route22WildMons:
 	def_grass_wildmons 25 ; encounter rate
 	db  3, RATTATA
 IF DEF(_RED)
-	db  3, NIDORAN_M
 	db  4, RATTATA
+	db  3, MANKEY
 	db  4, NIDORAN_M
-	db  2, RATTATA
-	db  2, NIDORAN_M
-	db  3, SPEAROW
-	db  5, SPEAROW
 	db  3, NIDORAN_F
+	db  3, SPEAROW
+	db  2, NIDORAN_M
+	db  5, MANKEY
+	db  2, NIDORAN_F
 	db  4, NIDORAN_F
 ENDC
 IF DEF(_BLUE)
